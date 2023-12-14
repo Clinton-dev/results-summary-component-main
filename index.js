03e855671b2c5f20d1d5fe932bc4b6bc5b8b9875
clinton-dev <clintonwambugu@gmail.com>
2023-12-14 17:28:55 +0300

    Add individual category fields

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ import results from "./data.mjs";
 
 const summary = document.getElementById("summary-details-container");
 console.log(results);
+let categoryResults = ``;
+
+results.map((result, index) => {
+  categoryResults += `
+            <div class="summary__wrapper summary__wrapper__${index + 1}">
+             <div class="summary__icons ">
+                <img src=${result.icon} />
+                <p>${result.category}</p>
+             </div>
+             <p><strong>${result.score}</strong> / 100</p>
+            </div>
+    `;
+});
+
+summary.innerHTML = categoryResults;
