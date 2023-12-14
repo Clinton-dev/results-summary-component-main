abe99b9e52f02b626bdd21a5cc98b6bbf1fe85cf
ClintonW <clintonwambugu@gmail.com>
2023-12-14 22:38:27 +0300

    Add animations

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ console.log(results);
 let categoryResults = ``;
 
 results.map((result, index) => {
+  const delay = 0.3 * (index + 1);
   categoryResults += `
-            <div class="summary__wrapper summary__wrapper__${index + 1}">
+            <div class="summary__wrapper summary__wrapper__${
+              index + 1
+            }" style="animation-delay: ${delay}s">
              <div class="summary__icons ">
                 <img src=${result.icon} />
-                <p>${result.category}</p>
+                <p class="summary__icons__text__${index + 1}">${
+    result.category
+  }</p>
              </div>
              <p><strong>${result.score}</strong> / 100</p>
             </div>
@@ -17,3 +22,8 @@ results.map((result, index) => {
 });
 
 summary.innerHTML = categoryResults;
+
+// Try to fix the jumpy animation
+// document.querySelectorAll(".summary__wrapper").forEach((element) => {
+//   element.classList.add("show");
+// });
